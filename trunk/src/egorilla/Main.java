28a3021cb81f2b131a745de1b608df9f749b89a4
pitidecaner@gmail.com <pitidecaner@gmail.com@33208996-a120-11dd-a887-7ffe1a420f8d>
2008-10-25 18:11:55 +0000

    "Import Inicial"

---
+++ trunk/src/egorilla/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package egorilla;
+
+/**
+ *as
+ * @author pitidecaner
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void dummy (){
+        //this is just a test!
+    }
+}
