0e2ccc1cb798da1ce31031bf0ba997182a25942d
pitidecaner@gmail.com <pitidecaner@gmail.com@33208996-a120-11dd-a887-7ffe1a420f8d>
2008-12-14 22:21:34 +0000

    Cometarios, comentarios, comentarios

--- servidor/src/servidoregorilla/protocolo/QueryResolver.java
+++ servidor/src/servidoregorilla/protocolo/QueryResolver.java
@@ -7,15 +7,15 @@ package servidoregorilla.protocolo;
 
 import Networking.PeerConn;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import servidoregorilla.Datos.ListaArchivos;
 import servidoregorilla.paquete.Archivo;
 import servidoregorilla.paquete.Query;
 import servidoregorilla.paquete.QueryAnswer;
 
 /**
- *
+ * Esta clase implementa el hilo que se ejecuta para resolver una consulta.
+ * Se encarga de localizar los datos requeridos por el cliente y reenviarselos
+ * 
  * @author pitidecaner
  */
 public class QueryResolver extends Thread{
@@ -24,13 +24,29 @@ public class QueryResolver extends Thread{
     private PeerConn _con;
     private Query _query;
     
-    
+    /**
+     * La instancia necesita los siguientes datos poder resolver las consultas.
+     * 
+     * @param l la lista de archivos global dados de alta en este servidor.
+     * @param q la consulta recibida por el cliente.
+     * @param con la conexion por la que contestar.
+     */
     public QueryResolver(ListaArchivos l, Query q, PeerConn con){
         _listaglobal = l;
         _con = con;
         _query = q;
     }
     
+    /**
+     * el hilo que realiza la consulta:
+     * // busca coincidencias en la BBDD
+       
+        // TODO: mejorar esto,
+        // la conjuncion de los dos conjuntos obtenidos es la solución. 
+        // por ahora solo busca por nombre y tiene que ser exactamente igual
+
+        // compone respuesta
+     */
     public void run(){
         
         Archivo[] lista;
@@ -48,15 +64,18 @@ public class QueryResolver extends Thread{
             // enviar a cliente.
             _con.sendObject(respuesta);
         } catch (IOException ex) {
-         // TODO: tenemos un problema, hacer algo, habra que darlo de baja, o no?
-            // si se ha interrumpido la conexion se dara cuenta el peerconnpool
-            // si 
-    }
+          // do nothing. todo ha ido bien y ya le dara de baja el peerconnpool
+        }
 
         // si todo ha ido bien, hemos acabado aki
         _con.setReady();
     }
 
+    /**
+     * La versión para los queries es 2
+     * 
+     * @return 2
+     */
     public int getVersion() {
         return 2;
     }
