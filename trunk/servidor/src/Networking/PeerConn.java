54612c1eda154efe445f9251b3858d80d05135a0
Salcedonia <Salcedonia@33208996-a120-11dd-a887-7ffe1a420f8d>
2008-12-15 19:07:58 +0000

    Comentarios añadidos y limpieza de código.

--- trunk/servidor/src/Networking/PeerConn.java
+++ trunk/servidor/src/Networking/PeerConn.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Networking;
 
 import java.io.IOException;
@@ -10,8 +9,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
-import servidoregorilla.server.Server;
 
+/*****************************************************************************/
 /**
  * Clase que hace las veces de un wrapper para encapsular todas las comunicaciones 
  * con un peer determinado, evitando el uso de streams y otras clases en todas 
@@ -24,11 +23,11 @@ public class PeerConn {
 
     // ATRIBUTOS
     private Socket _conexion; // Conexión entre los peers
-    private ObjectInputStream  _objetoEntrada; // Flujo de entrada para objetos
-    private ObjectOutputStream _objetoSalida; // Flujo de salida para objetos
-    
-    private boolean _ready;
+    private ObjectInputStream _objetoEntrada; // Flujo de entrada para objetos
+    private ObjectOutputStream _objetoSalida; // Flujo de salida para objetos  
+    private boolean _listo; // Indica si tiene algo que leer de la conexion
 
+/*****************************************************************************/
     /**
      * Constructor de la clase PeerConn. Se genera una clase que representa la 
      * conexión recibida. Esta conexion se puede utilizar para enviar y recibir 
@@ -37,29 +36,38 @@ public class PeerConn {
      * @param conexion Socket abierto con peer.
      * @throws java.io.IOException Se lanza en caso de error con la conexión.
      */
-    public PeerConn(Socket conexion) throws IOException{
+    public PeerConn(Socket conexion) throws IOException {
+
         _conexion = conexion;
 
         _objetoEntrada = new ObjectInputStream(conexion.getInputStream());
         _objetoSalida = new ObjectOutputStream(conexion.getOutputStream());
     }
 
+/*****************************************************************************/
     /**
      * Devuelve la dirección IP asociada a la conexión establecida.
      * 
      * @return La dirección IP asociada a la conexión establecida.
      */
     public String getIP() {
-        
+
         InetAddress adr = _conexion.getInetAddress();
-       
         return adr.getHostName();
     }
 
-    public boolean ready() {
-        return _ready;
+/*****************************************************************************/
+    /**
+     * Devuelve el valor de la variable _listo.
+     * 
+     * @return El valor de la variable _listo.
+     */
+    public boolean getListo() {
+
+        return _listo;
     }
- 
+
+/*****************************************************************************/
     /**
      * Lee un integer transmitido por el peer. Esta lectura es bloqueante, por 
      * lo que si no se produce, el hilo se quedará bloqueado en este punto sin 
@@ -68,22 +76,26 @@ public class PeerConn {
      * @return El entero recibido por el conexion.
      * @throws java.io.IOException Se lanza en caso de error con la conexión.
      */
-    public synchronized int reciveInt() throws IOException{
+    public synchronized int recibirEntero() throws IOException {
+
         _conexion.setSoTimeout(0);
         return _objetoEntrada.readInt();
     }
-    
+
+/*****************************************************************************/
     /**
-     * Envía un número entero con signo por el conexion.
+     * Envía un número entero con signo por la conexión.
      * 
      * @param num en entero a enviar por el conexion.
      * @throws java.io.IOException Se lanza en caso de error con la conexión.
      */
-    public synchronized void sendInt(int num) throws IOException{
-     _conexion.setSoTimeout(0);
+    public synchronized void enviarEntero(int num) throws IOException {
+
+        _conexion.setSoTimeout(0);
         _objetoSalida.writeInt(num);
     }
-    
+
+/*****************************************************************************/
     /**
      * Lee un objeto transimitido por el peer. Esta lectura es bloqueante, por 
      * lo que si no se produce, el hilo se quedará bloqueado en este punto sin 
@@ -94,56 +106,72 @@ public class PeerConn {
      * @throws java.lang.ClassNotFoundException Se lanza si el objeto no puede 
      * ser reconocido.
      */
-    public synchronized  Object reciveObject() throws IOException, ClassNotFoundException{
+    public synchronized Object recibirObjeto() throws IOException, ClassNotFoundException {
+
         _conexion.setSoTimeout(0);
-        return _objetoEntrada.readObject();   
+        return _objetoEntrada.readObject();
     }
-        /**
+
+/*****************************************************************************/
+    /**
      * Lee un objeto transimitido por el peer. Esta lectura es bloqueante, por
      * lo que si no se produce, el hilo se quedará bloqueado en este punto sin
      * progresar hasta recibir la instancia.
      *
-     * @param  timeout, milisegundos a esperar antes de lanzar excecpion SocketTimeoutException
+     * @param  timeout Milisegundos a esperar antes de lanzar excecpion SocketTimeoutException
+     * 
      * @return El objeto leído del peer.
+     * 
      * @throws java.io.IOException Se lanza en caso de error de transmisión.
      * @throws java.lang.ClassNotFoundException Se lanza si el objeto no puede
      * ser reconocido.
      */
-    public synchronized  Object reciveObject(int timeout) throws IOException, ClassNotFoundException{
+    public synchronized Object recibirObjeto(int timeout) throws IOException, ClassNotFoundException {
+
         _conexion.setSoTimeout(1000);
         return _objetoEntrada.readObject();
     }
 
-
-   /**
-    * Envía un objeto por el conexion. 
-    * 
-    * @param o Objeto a transmitir, debe ser serializable.
-    * @throws java.io.IOException Se lanza en caso de error con la conexión.
-    */
-    public void sendObject(Object o) throws IOException{
-         _conexion.setSoTimeout(0);
+/*****************************************************************************/
+    /**
+     * Envía un objeto por el conexion. 
+     * 
+     * @param o Objeto a transmitir, debe ser serializable.
+     * @throws java.io.IOException Se lanza en caso de error con la conexión.
+     */
+    public void enviarObjeto(Object o) throws IOException {
+        _conexion.setSoTimeout(0);
         _objetoSalida.writeObject(o);
     }
-    
+
+/*****************************************************************************/
     /**
      * Finaliza conexión con peer terminando toda comunicación con éste, cerrando
      * la conexión y los flujos de entrada y de salida.
      * 
      * @throws java.io.IOException Se lanza en caso de error.
      */
-    public synchronized void close() throws IOException{
+    public synchronized void cerrarComunicacion() throws IOException {
         _objetoEntrada.close();
         _objetoSalida.close();
         _conexion.close();
     }
-    
-    
-    public void setReady(){
-        _ready =true;
+
+/*****************************************************************************/
+    /**
+     * Establece el atributo _listo a cierto.
+     */
+    public void listo() {
+
+        _listo = true;
     }
-    
-    public void setWait(){
-        _ready = false;
+
+/*****************************************************************************/
+    /**
+     * Establece el atributo _listo a falso.
+     */
+    public void espera() {
+
+        _listo = false;
     }
 }
