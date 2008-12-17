eb84c99dca6f68670cecc93d4dac2eb1731e8138
pitidecaner <pitidecaner@33208996-a120-11dd-a887-7ffe1a420f8d>
2008-12-17 21:57:42 +0000

    listener de evetos en jtable1 (menudo nombre) este es el que dice "bajate este archivo"

---
+++ cliente/Cliente-eGorilla/src/presentacion/buscador/SelectionListener.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package presentacion.buscador;
+
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+/**
+ *
+ * @author Pitidecaner
+ */
+public class SelectionListener implements ListSelectionListener {
+        JTable table;
+
+        // It is necessary to keep the table since it is not possible
+        // to determine the table from the event's source
+        SelectionListener(JTable table) {
+            this.table = table;
+        }
+        public void valueChanged(ListSelectionEvent e) {
+            if (e.getSource() == table.getColumnModel().getSelectionModel()
+                   && table.getColumnSelectionAllowed() ){
+                // Row selection changed
+                int first = e.getFirstIndex();
+                int last = e.getLastIndex();
+            }
+
+
+
+//
+//            // If cell selection is enabled, both row and column change events are fired
+//            if (e.getSource() == table.getSelectionModel()
+//                  && table.getRowSelectionAllowed()) {
+//                // Column selection changed
+//                int first = e.getFirstIndex();
+//                int last = e.getLastIndex();
+//
+//            if (e.getValueIsAdjusting()) {
+//                // The mouse button has not yet been released
+//            }
+        }
+    }
+
