6f6aa2bc37589bf48853452f6073901df2117b04
pitidecaner <pitidecaner@33208996-a120-11dd-a887-7ffe1a420f8d>
2008-12-17 19:55:53 +0000

    modificaciones PeerConn cliente que conecta

--- cliente/Cliente-eGorilla/src/control/ControlAplicacion.java
+++ cliente/Cliente-eGorilla/src/control/ControlAplicacion.java
@@ -32,8 +32,10 @@ public class ControlAplicacion {
     
     public static void conectar() throws IOException{
               // Crea una conexion de prueba
-            Socket conexion = new Socket("127.0.0.1", 6969);
-            
+           // Socket conexion = new Socket("127.0.0.1", 6969);
+        Socket conexion = new Socket("192.168.1.32", 6969);
+
+
             _conn = new PeerConn(conexion);
           
             DatosCliente me = new DatosCliente();
