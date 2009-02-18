a26214b30b86ff81c22ae09e30ce279cde426bb9
pitidecaner <pitidecaner@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-02-18 17:05:13 +0000

    comenzando la refactorizacion pensando en la arquitectura creada

---
+++ trunk/cliente/src/gestorDeRed/Mensaje.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestorDeRed;
+
+/**
+ *
+ * @author pc2
+ */
+public interface Mensaje {
+
+}
