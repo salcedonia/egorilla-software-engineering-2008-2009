34b69bfcd8e023b651c69c51957b7bb26694ad4a
pitidecaner <pitidecaner@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-03-16 20:35:32 +0000

    network manager

---
+++ branches/luis/NetworkManager/src/gestorDeRed/GestorDeRed.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestorDeRed;
+
+import java.net.Inet4Address;
+
+/**
+ *
+ * @author pitidecaner
+ */
+public interface GestorDeRed<E>{
+
+    /**
+     * A partir del momento en el que se llama a este método se expande un
+     * hilo el cual estará atento de escuchar un determinado puerto.
+     * dicho puerto se deja a discrección de quien instancie la implementación
+     * de esta interface.
+     */
+    public void comienzaEscucha();
+
+    /**
+     * envía un paquete de tipo E a la dirección especificada
+     * @param var el paquete a enviar
+     * @param destino destino en formato ip v4
+     * @param port puerto a enviar
+     */
+    public void envia(E var, Inet4Address destino, int port);
+    /**
+     * envía un paquete de tipo E a la dirección especificada
+     * @param var el paquete a enviar
+     * @param host el nombre de host o ip
+     * @param port el puerto
+     */
+    public void envia(E var, String host, int port);
+
+    /**
+     * registra un receptor que se hara cargo de los mensajes recibidos por el
+     * puerto especificado en el constructor
+     *
+     * @param r el receptor debe implementar esta interface.
+     */
+    public void registraReceptor(Receptor<E> r);
+}
