a921c516dd8117d32808c38cafd2692cd46dad3c
f.javier.s.pardo <f.javier.s.pardo@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-03-29 23:37:23 +0000

    He añadido el enganche con el Gestor de Configuracion. La configuracion del cliente se lee de los ficheros: -cliente.properties -cliente_default.properties

--- Cliente/src/main/Main.java
+++ Cliente/src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
+import gestorDeConfiguracion.ControlConfiguracionCliente;
 import control.*;
+import gestorDeConfiguracion.ControlConfiguracionClienteException;
 import gui.GUIConsola;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -18,17 +20,19 @@ public class Main {
      * 
      * @param args Argumentos de la aplicación de la línea de comandos.
      */
-    public static void main(String[] args){
+    public static void main(String[] args) throws ControlConfiguracionClienteException{
 
         String cad = null;
+        
+        ControlConfiguracionCliente oCtrlConfigCliente = ControlConfiguracionCliente.obtenerInstancia("cliente.properties", "cliente_default.properties");
+        int iPuerto = Integer.parseInt(oCtrlConfigCliente.obtenerPropiedad("Puerto"));
+        ServidorFicheros fs = new ServidorFicheros(iPuerto);
 
-        // TODO: ojo con esto, el puerto de ecucha esta puesto muy mal
-        // hay que leerlo de la configuracion. properties o lo que sea
-        ServidorFicheros fs = new ServidorFicheros(4000);
+//        ServidorFicheros fs = new ServidorFicheros(4000);
         fs.start();
 
-        String nombreDirectorio = "compartidos";
-        ControlAplicacion.compartidos(nombreDirectorio);
+//        String nombreDirectorio = "compartidos";
+        ControlAplicacion.compartidos(oCtrlConfigCliente.obtenerPropiedad("Dir_Compartidos"));
         
         // Mostramos la interfaz de consola
         try {
