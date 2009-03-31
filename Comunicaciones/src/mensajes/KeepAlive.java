28d32c5dbedb94d7169a6d876a30c70f94a3fbec
pitidecaner <pitidecaner@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-03-31 18:53:14 +0000

    la foto correcta del gestor de red en el diseño del sistema

--- Comunicaciones/src/mensajes/KeepAlive.java
+++ Comunicaciones/src/mensajes/KeepAlive.java
@@ -6,9 +6,40 @@
 package mensajes;
 
 /**
+ * El mensaje keepalive notifica que la conexion sigue establecida
+ * para la implementación no orientada a conexion.
+ * 
+ * el envio de este mensaje implica que el otro host sigue en linea
+ * y ademas especifica cuanto tiempo pasa hasta el siguiente envio
+ * 
+ * el otro host esperará un tiempo igual a 3 veces
+ * el tiempo especificado antes de entrar en panico.
  *
  * @author Luis Ayuso
  */
-public class KeepAlive {
+public class KeepAlive implements Mensaje{
+
+    private String _destinoIP;
+    private int _destinoPuerto;
+    
+    public long segundosCadaKeepAlive;
+    
+    
+    public TipoMensaje getTipoMensaje() {
+        return TipoMensaje.keepAlive;
+    }
+
+    public void setDestino(String destino, int puerto) {
+        _destinoIP = destino;
+        _destinoPuerto = puerto;
+    }
+
+    public String destino() {
+        return _destinoIP;
+    }
+
+    public int puerto() {
+        return _destinoPuerto;
+    }
 
 }
--- Comunicaciones/src/mensajes/TipoMensaje.java
+++ Comunicaciones/src/mensajes/TipoMensaje.java
@@ -25,5 +25,6 @@ public enum TipoMensaje {
     RespuestaPeticionConsulta,
     RespuestaPeticionDescarga,
     DatosCliente,
-    Bienvenido
+    Bienvenido,
+    keepAlive
 }
