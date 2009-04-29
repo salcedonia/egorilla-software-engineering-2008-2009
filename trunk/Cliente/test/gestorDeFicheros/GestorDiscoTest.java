a202911a0d9d67b48ef09389265bacfe0acd2d13
munsuri <munsuri@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-04-29 08:02:48 +0000

    Subida la base de los test JUnit v4.5 y añadidas cosillas al ensamblador

---
+++ trunk/Cliente/test/gestorDeFicheros/GestorDiscoTest.java
@@ -0,0 +1,54 @@
+package gestorDeFicheros;
+
+import static org.junit.Assert.*;
+import junit.framework.JUnit4TestAdapter;
+import org.junit.*;
+
+/**
+ */
+public class GestorDiscoTest {
+    
+
+
+  /*  @Before
+  public void inicializar(){
+
+  }*/
+
+  public static junit.framework.Test suite(){
+    return new JUnit4TestAdapter( GestorDiscoTest.class );
+  }
+
+    /*@BeforeClass
+    public static void setUpClass() throws Exception {
+        
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        
+    }
+
+    @After
+    public void tearDown() {
+    }*/
+
+    /**
+     */
+    @Test
+    public void test1() {
+
+    }
+
+    /**
+     */
+    @Ignore
+    @Test
+    public void test2() {
+    }
+
+}
