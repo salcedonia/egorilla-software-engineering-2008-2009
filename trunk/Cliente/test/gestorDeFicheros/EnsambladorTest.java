96c6d720b493be13ed51d3645c4b2ad9a8f34cd9
munsuri <munsuri@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-04-30 06:46:26 +0000

    Nuevos test subidos y reestructuración de código

---
+++ trunk/Cliente/test/gestorDeFicheros/EnsambladorTest.java
@@ -0,0 +1,69 @@
+package gestorDeFicheros;
+
+import static org.junit.Assert.*;
+import junit.framework.JUnit4TestAdapter;
+import org.junit.*;
+
+/**
+ */
+public class EnsambladorTest {
+    
+
+
+  /*  @Before
+  public void inicializar(){
+
+  }*/
+
+  public static junit.framework.Test suite(){
+    return new JUnit4TestAdapter( EnsambladorTest.class );
+  }
+
+    /*@BeforeClass
+    public static void setUpClass() throws Exception {
+        
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+        
+    }
+
+    @After
+    public void tearDown() {
+    }*/
+
+    /**
+     */
+    @Test
+    public void testConstructor() {
+
+      //Establecer las rutas relativas donde coloco los archivos de prueba
+      //como son private necesito hacer el reflect
+
+      GestorDisco gestorDisco = new GestorDisco();
+
+      Ensamblador ensamblador = gestorDisco.getEnsamblador();
+
+      String directorioTest = "test//gestorDeFicheros//";
+      String nombreFileComJar = "comunicaciones.jir";
+      File fileComJar = new File( directorioTest+nombreFileComJar );
+      String hashAux = MD5Sum.getFileMD5Sum( fileComJar );
+      Archivo archivoComJar = new Archivo( fileComJar.getName(), hashAux, fileComJar.length(), 
+        TipoArchivo.ARCHIVO);
+      System.out.println( archivoComJar.toString() );
+      ensamblador.nuevoArchivoTemporal( archivoComJar );
+    }
+
+    /**
+     */
+    @Ignore
+    @Test
+    public void test2() {
+    }
+
+}
--- trunk/Cliente/test/gestorDeFicheros/GestorDiscoTest.java
+++ trunk/Cliente/test/gestorDeFicheros/GestorDiscoTest.java
@@ -40,8 +40,31 @@ public class GestorDiscoTest {
     /**
      */
     @Test
-    public void test1() {
+    public void testConstructor() {
 
+      //Establecer las rutas relativas donde coloco los archivos de prueba
+      //como son private necesito hacer el reflect
+
+      GestorDisco gestorDisco = new GestorDisco();
+      assertEquals( gestorDisco.getListaArchivosTemporales() != null, true );
+      assertEquals( gestorDisco.getListaArchivosCompletos() != null, true );
+      assertEquals( gestorDisco.getListaArchivosTodos() != null, true );
+      //Comprobar el numero de archivo en estas.
+
+      assertEquals( gestorDisco.getManejarIndices() != null, true );
+      assertEquals( gestorDisco.getManejarListaArchivos() != null, true );
+
+      assertEquals( gestorDisco.getFragmentador() != null, true );
+      assertEquals( gestorDisco.getEnsamblador() != null, true );
+
+      assertEquals( gestorDisco.getDirectorioTemporales() != null, true );
+      assertEquals( gestorDisco.getDirectorioCompletos() != null, true );
+      //comprobar q son los de por defecto, añadiendo la ruta para las pruebas
+
+      assertEquals( gestorDisco.getExtesionIndices() != null, true );
+      assertEquals( gestorDisco.getExtesionFicheroTemporal() != null, true );
+      //comprobar q son los de por defecto
+      assertEquals( gestorDisco.getTamanioBytesFragmento(), 512 ); //puede variar!
     }
 
     /**
