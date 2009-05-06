71f91c5dbc7d0475959431b1fb9bd4bd756aaee3
mercedesbernalperez <mercedesbernalperez@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-05-06 00:34:47 +0000

    GUIPanelCompartidos

--- Cliente/src/gui/grafica/compartidos/GUIPanelCompartidos.java
+++ Cliente/src/gui/grafica/compartidos/GUIPanelCompartidos.java
@@ -1,17 +1,25 @@
 package gui.grafica.compartidos;
 
+import datos.Archivo;
+import gestorDeFicheros.GestorCompartidos;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import mensajes.serverclient.ListaArchivos;
+import org.jfree.data.xy.Vector;
 
 /**
  * Clase que gestiona el panel de compartidos en la ventana principal de la
  * aplicación.
  * 
- * @author Javier Salcedo
+ * @author Mercedes Bernal
  */
 public class GUIPanelCompartidos extends JPanel {
 
@@ -30,6 +38,12 @@ public class GUIPanelCompartidos extends JPanel {
     private JScrollPane _scrollPaneContenido;
     private JSplitPane _splitPanel;
     private JTable _tablaContenido;
+    
+    private GestorCompartidos _gestorDeCompartidos;
+    /**
+     * Contiene el resultado de una lista de archivos compartidos.
+     */
+    private PanelCompartidos _panelCompartidos;
     
     /** 
      * Constructor de la clase PanelCompartidos. 
@@ -60,16 +74,47 @@ public class GUIPanelCompartidos extends JPanel {
         _panelArbol.setLayout(new BoxLayout(_panelArbol, BoxLayout.LINE_AXIS));
         
         DefaultMutableTreeNode treeNode1 = new DefaultMutableTreeNode("Compartidos");      
-        DefaultMutableTreeNode treeNode2 = new DefaultMutableTreeNode("Todos los archivos compartidos");
+        /*DefaultMutableTreeNode treeNode2 = new DefaultMutableTreeNode("Todos los archivos compartidos");
         treeNode1.add(treeNode2);
         treeNode2 = new DefaultMutableTreeNode("Archivos completados");
         treeNode1.add(treeNode2);
         treeNode2 = new DefaultMutableTreeNode("Archivos incompletos");
         treeNode1.add(treeNode2);
         treeNode2 = new DefaultMutableTreeNode("Directorios compartidos");
-        treeNode1.add(treeNode2);
+        treeNode1.add(treeNode2);*/
+        
         _explorador.setModel(new DefaultTreeModel(treeNode1));
         _scrollPaneArbol.setViewportView(_explorador);
+        
+        
+        _explorador.addMouseListener(new MouseListener() {
+
+            @Override
+            public void mousePressed(MouseEvent arg0) {
+                System.out.println("mousePressed");
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("mouseClicked");
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println("mouseReleased");
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                System.out.println("mouseEntered");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                System.out.println("mouseExited");
+            }
+
+        });
 
         _panelArbol.add(_scrollPaneArbol);
         _splitPanel.setLeftComponent(_panelArbol);
@@ -77,12 +122,26 @@ public class GUIPanelCompartidos extends JPanel {
         _panelContenido.setLayout(new BoxLayout(_panelContenido, BoxLayout.LINE_AXIS));
         _scrollPaneContenido.setAutoscrolls(true);
         
-        _tablaContenido.setModel(new DefaultTableModel(
-                new Object[][]{}, new String[]{"Nombre", "Tamaño", "Tipo", "Identificador", "Carpeta"}));
-        _tablaContenido.setPreferredSize(new Dimension(300, 64));
+        DefaultTableModel modelo = new DefaultTableModel(new Object[][]{}, new String[]{"Nombre", "Tamaño", "Tipo", "Identificador"});
+        listarArchivosCompartidos(modelo);
+        
+        _tablaContenido.setModel(modelo);
+        //_tablaContenido.setPreferredSize(new Dimension(300, 140));
         _scrollPaneContenido.setViewportView(_tablaContenido);
         _panelContenido.add(_scrollPaneContenido);
         _splitPanel.setRightComponent(_panelContenido);
         add(_splitPanel);
     }
+    
+    private void listarArchivosCompartidos(DefaultTableModel modelo){
+        ListaArchivos listacompartidos = _gestorDeCompartidos.getInstancia().getArchivosCompartidos();
+        
+        for(int i=0; i<listacompartidos.size();i++){
+            //_tablaContenido.add
+            Archivo archivo = listacompartidos.get(i);
+            Object[] objeto = {archivo.getNombre(), new Long(archivo.getSize()), archivo.getTipo().toString(), archivo.getHash().toString()};
+            modelo.addRow(objeto);
+        }
+    }
+    
 }
