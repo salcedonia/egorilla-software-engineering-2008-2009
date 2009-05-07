7d43f1f1c0c20391424f5d148d733306cd3455ce
munsuri <munsuri@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-05-07 09:27:54 +0000

    Bats adaptados para los laboratorios

--- trunk/Cliente/ejC.bat
+++ trunk/Cliente/ejC.bat
@@ -1,1 +1,1 @@
-java -cp bin;lib/log4j-1.2.15.jar;lib/jargs.jar;lib/jfreechart-1.0.13.jar;lib/jcommon-1.0.16.jar main.Main %1 %2
+java -cp bin;lib/log4j-1.2.15.jar;lib/jargs.jar;lib/jfreechart-1.0.13.jar;lib/jcommon-1.0.16.jar main.Main -i grafico
