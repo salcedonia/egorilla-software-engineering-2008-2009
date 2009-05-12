050257169df25fba1ac40698455b8017284c7929
luis.f.Ayuso <luis.f.Ayuso@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-05-12 10:49:14 +0000

    rediseño en curso, copia NO funciona

---
+++ Cliente/src/peerToPeer/egorilla/GestorMensajes.java
@@ -0,0 +1,57 @@
+package peerToPeer.egorilla;
+
+import gestorDeErrores.ControlDeErrores;
+import gestorDeErrores.ErrorEGorilla;
+import gestorDeRed.GestorDeRed;
+import gestorDeRed.NetError;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import mensajes.Mensaje;
+
+/**
+ * gestiona la salida de mensajes
+ * TODOS los mensajes salen desde esta clase.
+ * si se implementa algún control de flujo de subida DEBE ser aqui
+ * @author Luis Ayuso
+ */
+class GestorMensajes extends Thread{
+    private Queue<Mensaje> _cola;
+    private GestorDeRed<Mensaje> _red;
+
+    public GestorMensajes(GestorDeRed<Mensaje> red) {
+        _cola = new LinkedList<Mensaje>();
+        _red = red;
+        this.setName("Cola de Mensajes");
+        this.run();
+    }
+    synchronized  void addMensajeParaEnviar(Mensaje msj){
+        _cola.add(msj);
+        this.notify();
+    }
+
+    @Override
+    public void run() {
+        try {
+            wait();
+            while (!_cola.isEmpty()) {
+                Mensaje msj = _cola.poll();
+                try {
+                    _red.envia(msj, msj.ipDestino(), msj.puertoDestino());
+                } catch (NetError ex) {
+                    ControlDeErrores.getInstancia().registrarError(ErrorEGorilla.ERROR_RED,
+                            "error al enviar mensaje a " + msj.ipDestino() + ": " +
+                            ex.getError());
+                }
+            }
+        } catch (InterruptedException ex) {
+            // paramos y listo!
+        }
+    }
+
+    synchronized void parar(){
+        _cola.clear();
+        this.interrupt();
+    }
+}
