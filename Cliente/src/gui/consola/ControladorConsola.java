f6e91fb67dc9ec78a306aff9c6b69555be2772d1
mercedesbernalperez <mercedesbernalperez@33208996-a120-11dd-a887-7ffe1a420f8d>
2009-05-20 17:51:05 +0000

    Cabecera GNU

--- Cliente/src/gui/consola/ControladorConsola.java
+++ Cliente/src/gui/consola/ControladorConsola.java
@@ -1,3 +1,19 @@
+/* 
+	This file is part of eGorilla.
+
+    eGorilla is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    eGorilla is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eGorilla.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package gui.consola;
 
 import datos.Archivo;
@@ -10,7 +26,7 @@ import peerToPeer.GestorP2P;
  * notifica a la lógica de la aplicación para que ésta actúe en consecuencia.
  * 
  * @author Luis Ayuso, Ivan Munsuri, Javier Salcedo, Jose Miguel Guerrero
- * Modificado por Javier Sánchez.
+ * Modificado por Javier Sánchez, Mercedes Bernal
  */
 public class ControladorConsola {
 
